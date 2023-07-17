590c14748a9081f5d128a30caf665b92c54039a1
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2023-07-17 23:40:53 +0600

    m10live

---
+++ lib/home.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('News Feed'),
+      ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+      if (orientation == Orientation.portrait) {
+        return Potrait();
+      } else {
+        return Landscape();
+      }
+    },
+    ));
+  }
+    
+  }
+class NewsArticle {
+  final String imageUrl;
+
+  NewsArticle({required this.imageUrl});
+}
+
+
+
+
+  
+  class Potrait extends StatefulWidget {
+    const Potrait({Key? key}) : super(key: key);
+  
+    @override
+    State<Potrait> createState() => _PotraitState();
+  }
+
+  
+  class _PotraitState extends State<Potrait> {
+
+    final List<NewsArticle> news = [
+      NewsArticle(imageUrl: 'https://northyorkshireccg.nhs.uk/wp-content/uploads/2021/02/150x150.png'),
+      NewsArticle(imageUrl: 'https://northyorkshireccg.nhs.uk/wp-content/uploads/2021/02/150x150.png'),
+      NewsArticle(imageUrl: 'https://northyorkshireccg.nhs.uk/wp-content/uploads/2021/02/150x150.png'),
+      NewsArticle(imageUrl: 'https://northyorkshireccg.nhs.uk/wp-content/uploads/2021/02/150x150.png'),
+      NewsArticle(imageUrl: 'https://northyorkshireccg.nhs.uk/wp-content/uploads/2021/02/150x150.png'),
+      NewsArticle(imageUrl: 'https://northyorkshireccg.nhs.uk/wp-content/uploads/2021/02/150x150.png'),
+      NewsArticle(imageUrl: 'https://northyorkshireccg.nhs.uk/wp-content/uploads/2021/02/150x150.png'),
+      NewsArticle(imageUrl: 'https://northyorkshireccg.nhs.uk/wp-content/uploads/2021/02/150x150.png'),
+      NewsArticle(imageUrl: 'https://northyorkshireccg.nhs.uk/wp-content/uploads/2021/02/150x150.png'),
+      // Add more articles as needed
+    ];
+
+    @override
+    Widget build(BuildContext context) {
+      return Container(
+        child: ListView.builder(
+          itemCount: news.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Column(
+           children: [
+               Container(
+                   width: 150,
+                   height: 150,
+                   child: Image.network(
+                     news[index].imageUrl,
+                   )),
+             Divider(
+               color: Colors.black,
+             )
+           ],
+              ),
+            );
+
+          }
+
+        ),
+      );
+    }
+  }
+
+
+  
+  
+  class Landscape extends StatefulWidget {
+    const Landscape({Key? key}) : super(key: key);
+
+
+  
+    @override
+    State<Landscape> createState() => _LandscapeState();
+  }
+  
+  class _LandscapeState extends State<Landscape> {
+    final List<NewsArticle> news = [
+      NewsArticle(imageUrl: 'https://northyorkshireccg.nhs.uk/wp-content/uploads/2021/02/150x150.png'),
+      NewsArticle(imageUrl: 'https://northyorkshireccg.nhs.uk/wp-content/uploads/2021/02/150x150.png'),
+      NewsArticle(imageUrl: 'https://northyorkshireccg.nhs.uk/wp-content/uploads/2021/02/150x150.png'),
+      NewsArticle(imageUrl: 'https://northyorkshireccg.nhs.uk/wp-content/uploads/2021/02/150x150.png'),
+      NewsArticle(imageUrl: 'https://northyorkshireccg.nhs.uk/wp-content/uploads/2021/02/150x150.png'),
+      NewsArticle(imageUrl: 'https://northyorkshireccg.nhs.uk/wp-content/uploads/2021/02/150x150.png'),
+      NewsArticle(imageUrl: 'https://northyorkshireccg.nhs.uk/wp-content/uploads/2021/02/150x150.png'),
+      NewsArticle(imageUrl: 'https://northyorkshireccg.nhs.uk/wp-content/uploads/2021/02/150x150.png'),
+      NewsArticle(imageUrl: 'https://northyorkshireccg.nhs.uk/wp-content/uploads/2021/02/150x150.png'),
+      // Add more articles as needed
+    ];
+    @override
+    Widget build(BuildContext context) {
+      return  Container(
+        child: ListView.builder(
+            itemCount: news.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(height: 50,width: 50,),
+                    Padding(
+                      padding: const EdgeInsets.all(50.0),
+                      child: Container(
+                          width: 150,
+                          height: 150,
+                          child: Image.network(
+                            news[index].imageUrl,
+                          )),
+                    ),
+                    Divider(
+                      color: Colors.black,
+                    ),
+                    
+                    Container(
+                        width: 150,
+                        height: 150,
+                        child: Image.network(
+                          news[index].imageUrl,
+                        )),
+                    SizedBox(height: 50,width: 50,),
+                  ],
+                ),
+              );
+
+            }
+
+        ),
+      );;
+    }
+  }
+  
+  
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '',
+      home:Home(),
+    );
+  }
+}
